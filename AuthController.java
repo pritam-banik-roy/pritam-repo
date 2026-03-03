346cbe757f845d723045bdc110d06b151eb25103
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-04 01:19:15 +0530

    Create AuthController.java

---
+++ AuthController.java
@@ -0,0 +1,57 @@
+package com.flightreservation.controller;
+
+import com.flightreservation.model.User;
+import com.flightreservation.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String register(@Valid @ModelAttribute("user") User user,
+                           BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "signup";
+        }
+
+        userService.register(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user,
+                        HttpSession session,
+                        Model model) {
+
+        try {
+            User dbUser = userService.login(user.getEmail(), user.getPassword());
+            session.setAttribute("loggedUser", dbUser);
+            return "redirect:/search";
+        } catch(Exception e) {
+            model.addAttribute("error", "Invalid Credentials");
+            return "login";
+        }
+    }
+}
