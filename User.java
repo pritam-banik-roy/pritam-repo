e48478ac544aac16b70f3ff3398585b94aa317fa
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-04 01:01:34 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,18 +1,31 @@
-import javax.validation.constraints.*;
+package com.flightreservation.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 public class User {
 
     private int id;
 
-    @NotEmpty(message="Email is required")
-    @Email(message="Invalid email format")
+    @Email(message = "Invalid Email Format")
+    @NotEmpty(message = "Email is required")
     private String email;
 
-    @NotEmpty(message="Password is required")
+    @NotEmpty(message = "Password is required")
     private String password;
 
-    @NotEmpty(message="Role is required")
+    @NotEmpty(message = "Role is required")
     private String role;
 
-    // getters & setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
