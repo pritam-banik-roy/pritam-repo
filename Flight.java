9155eb691b649e94d48aa7c4ac9abbeebe6f51d4
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-04 00:47:53 +0530

    Create Flight.java

---
+++ Flight.java
@@ -0,0 +1,12 @@
+public class Flight {
+
+    private int id;
+    private String flightNumber;
+    private String source;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+    private double price;
+
+    // getters & setters
+}
