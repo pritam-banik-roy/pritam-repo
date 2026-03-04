608c3b19c68c2df355a76e63a227796393058f4e
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-04 22:57:29 +0530

    Update UserDAOImpl.java

--- UserDAOImpl.java
+++ UserDAOImpl.java
@@ -1,3 +1,34 @@
+// package com.flightreservation.dao;
+
+// import com.flightreservation.model.User;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.jdbc.core.BeanPropertyRowMapper;
+// import org.springframework.stereotype.Repository;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+// @Repository
+// public class FlightDAOImpl implements FlightDAO {
+
+//     @Autowired
+//     private JdbcTemplate jdbcTemplate;
+
+//     @Override
+//     public List<Flight> findFlights(String source, String destination) {
+
+//         String sql = "SELECT * FROM flight WHERE LOWER(source)=LOWER(?) AND LOWER(destination)=LOWER(?)";
+
+//         return jdbcTemplate.query(
+//                 sql,
+//                 new BeanPropertyRowMapper<>(Flight.class),
+//                 source,
+//                 destination
+//         );
+//     }
+// }
+
+
+=======================================================================================================================
+
 package com.flightreservation.dao;
 
 import com.flightreservation.model.User;
@@ -7,21 +38,34 @@ import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Repository
-public class FlightDAOImpl implements FlightDAO {
+public class UserDAOImpl implements UserDAO {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public List<Flight> findFlights(String source, String destination) {
+    public void save(User user) {
 
-        String sql = "SELECT * FROM flight WHERE LOWER(source)=LOWER(?) AND LOWER(destination)=LOWER(?)";
+        String sql = "INSERT INTO user(email,password,role) VALUES(?,?,?)";
 
-        return jdbcTemplate.query(
+        jdbcTemplate.update(
                 sql,
-                new BeanPropertyRowMapper<>(Flight.class),
-                source,
-                destination
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+
+    @Override
+    public User findByEmailAndPassword(String email,String password) {
+
+        String sql = "SELECT * FROM user WHERE email=? AND password=?";
+
+        return jdbcTemplate.queryForObject(
+                sql,
+                new BeanPropertyRowMapper<>(User.class),
+                email,
+                password
         );
     }
 }
