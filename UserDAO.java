c1f965c3ebe42228ab3517a63a6d52a748c977c0
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-04 23:58:01 +0530

    Update UserDAO.java

--- UserDAO.java
+++ UserDAO.java
@@ -1,9 +1,12 @@
 package com.flightreservation.dao;
 
 import com.flightreservation.model.User;
-import java.util.List;
 
 public interface UserDAO {
+
     void save(User user);
-    User findByEmailAndPassword(String email, String password);
+
+    User findByEmailAndPassword(String email,String password);
+
+    User findByEmail(String email);
 }
