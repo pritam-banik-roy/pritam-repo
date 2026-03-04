d5d59fc45311efb8f904359c31f036890201b198
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-05 00:55:32 +0530

    Update User.java

--- User.java
+++ User.java
@@ -79,39 +79,28 @@
 
 
 =======================================================================================================================================
-
-
 package com.flightreservation.model;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 public class User {
 
     private Long userId;
 
-    @NotEmpty(message = "Email is required")
+    @NotBlank(message = "Email is required")
     @Email(message = "Enter valid email format")
     private String email;
 
-    // @NotEmpty(message = "Password is required")
-    // @Size(min = 6, message = "Password must be at least 6 characters")
-    // @Pattern(
-    //     regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
-    //     message = "Password must contain uppercase, lowercase, number and special symbol"
-    // )
-    // private String password;
-
     @NotBlank(message = "Password is required")
     @Pattern(
-    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{6,}$",
-    message = "Password must be 6+ characters with uppercase, lowercase, number and special character"
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{6,}$",
+        message = "Password must be 6+ characters with uppercase, lowercase, number and special character"
     )
     private String password;
 
-    @NotEmpty(message = "Please select a role!")
+    @NotBlank(message = "Please select a role!")
     private String role;
 
     public Long getUserId() { return userId; }
