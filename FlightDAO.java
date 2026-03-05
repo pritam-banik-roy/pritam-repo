3198810b594685760e611e26c796cf416202c996
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-05 23:53:32 +0530

    Update FlightDAO.java

--- FlightDAO.java
+++ FlightDAO.java
@@ -4,5 +4,8 @@ import com.flightreservation.model.Flight;
 import java.util.List;
 
 public interface FlightDAO {
+
     List<Flight> findFlights(String source, String destination);
+
+    Flight getFlightById(int id);
 }
