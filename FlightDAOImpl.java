524f3cb936313a73d14d2f709d0e4974be4a7ca9
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-05 23:54:17 +0530

    Update FlightDAOImpl.java

--- FlightDAOImpl.java
+++ FlightDAOImpl.java
@@ -1,10 +1,12 @@
 package com.flightreservation.dao;
 
 import com.flightreservation.model.Flight;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 
 @Repository
@@ -15,11 +17,28 @@ public class FlightDAOImpl implements FlightDAO {
 
     @Override
     public List<Flight> findFlights(String source, String destination) {
-        //String sql = "SELECT * FROM flights WHERE source=? AND destination=?";
 
-        String sql = "SELECT * FROM flight WHERE LOWER(source)=LOWER(?) AND LOWER(destination)=LOWER(?)";
-        return jdbcTemplate.query(sql,
+        String sql =
+        "SELECT * FROM flight WHERE LOWER(source)=LOWER(?) AND LOWER(destination)=LOWER(?)";
+
+        return jdbcTemplate.query(
+                sql,
                 new BeanPropertyRowMapper<>(Flight.class),
-                source, destination);
+                source,
+                destination
+        );
+    }
+
+    @Override
+    public Flight getFlightById(int id) {
+
+        String sql =
+        "SELECT * FROM flight WHERE flight_id=?";
+
+        return jdbcTemplate.queryForObject(
+                sql,
+                new BeanPropertyRowMapper<>(Flight.class),
+                id
+        );
     }
 }
