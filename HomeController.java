4e40931784eb26cb212cf0f99aac1a51167a713b
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2026-03-06 01:53:49 +0530

    Update HomeController.java

--- HomeController.java
+++ HomeController.java
@@ -167,7 +167,7 @@ public class HomeController {
         }
 
         List<Flight> flights =
-                flightService.findFlights(
+                flightService.search(
                         searchFlight.getSource(),
                         searchFlight.getDestination()
                 );
